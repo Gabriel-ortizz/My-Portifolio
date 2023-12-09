8ebb333ecca7ef0b06baa3f5b5cb9ca1ba6fd546
Gabriel.ortizz <gabrielmd0graus@gmail.com>
2023-12-09 20:13:27 -0300

    refactoring

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const menuLinks = document.querySelectorAll(".menu__link");
 
 function changeTheme(){
   const currentTheme = rootHtml.getAttribute("data-theme");
@@ -10,4 +12,20 @@ function changeTheme(){
   toggleTheme.classList.toggle("bi-moon-stars")
 }
 
-toggleTheme.addEventListener("click",changeTheme)
+toggleTheme.addEventListener("click", changeTheme);
+
+accordionHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionActive = accordionItem.classList.contains("active");
+
+    accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+  })
+})
+
+menuLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  })
+})
